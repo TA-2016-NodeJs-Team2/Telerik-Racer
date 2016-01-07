0de89647cc9908dd954b5fa38cd262d7e7f2cb7c
mr.krustev <mr.krustev1994@gmail.com>
2016-01-07 17:49:58 +0200

    Add Cars controller and routing for endpoints.

---
+++ server/controllers/cars-controller.js
@@ -0,0 +1,117 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Car = mongoose.model('Car'),
+    User = mongoose.model('User'),
+    dateExt = require('../common/date-time-extensions');
+
+module.exports = function (carData) {
+    return {
+        getCarById: function (req, res, next) {
+            var id = req.body.id;
+            carData.getCarById(id)
+                .then(function (car) {
+                    res.json(car);
+                }, function (error) {
+                    res.status(error.status)
+                        .json({message: error.message});
+                });
+        },
+        getAllCars: function (req, res, next) {
+            carData.getAllCars()
+                .then(function (cars) {
+                    res.json(cars);
+                }, function (error) {
+                    res.status(error.status)
+                        .json({message: error.message});
+                });
+        },
+        getAllCarModels: function (req, res, next) {
+            carData.getAllCarModels()
+                .then(function (carModels) {
+                    res.json(carModels);
+                }, function (err) {
+                    res.status(error.status)
+                        .json({message: error.message});
+                })
+        },
+        // TODO: Decide whether we use two collections or one for cars.
+        //addCar: function (req, res, next) {
+        //    var car = req.body;
+        //
+        //    if (!req.body) {
+        //        res.status(400)
+        //            .json({
+        //                message: 'data was not provided!'
+        //            });
+        //        return;
+        //    }
+        //
+        //    //TODO:  || !(car.levelRequired < player.Level) - user controller needs - getCurrentPlayer
+        //    if (!(car.model)) {
+        //        res.status(400)
+        //            .json({
+        //                message: 'Username and password are required!'
+        //            });
+        //        return;
+        //    }
+        //
+        //    car.wheelsLevel = 1;
+        //    car.engineLevel = 1;
+        //    car.turbo = 1;
+        //    car.damage = 0;
+        //    car.dateCreated = dateExt.getUTCDate(new Date());
+        //    // TODO: Change according to model.
+        //    car.price = 1;
+        //    // TODO: Change according to model.
+        //    car.levelRequired = 1;
+        //
+        //    carData.save(car).then(function (readyCar) {
+        //        res.status(201)
+        //            .json({
+        //                carId: readyCar._id
+        //            });
+        //    }, function (error) {
+        //        res.status(error.status || 500)
+        //            .json({
+        //                message: error.message
+        //            });
+        //    });
+        //},
+        removeCar: function (req, res, next) {
+            if (!(req.body) || !(req.body.id)) {
+                res.status(400)
+                    .json({
+                        message: 'you should provide an ID!'
+                    });
+                return;
+            }
+
+            // authenticated User
+            var user = req.user;
+            var id = req.body.id;
+
+            // User should have role administrator.
+            console.log(user.role);
+            console.log(User.getRoles()[2]);
+
+            if (user.role !== User.getRoles()[2]) {
+                res.status(401)
+                    .json({
+                        message: 'permissions required!'
+                    });
+                return;
+            }
+
+            carData.remove(id)
+                .then(function (data) {
+                    res.json(data);
+                }, function (err) {
+                    res.status(err.status)
+                        .json({
+                            message: err.message
+                        });
+                });
+        }
+    }
+};
---
+++ server/routers/cars-router.js
@@ -0,0 +1,16 @@
+var express = require('express'),
+    router = express.Router(),
+    carsData = require('../data/data-cars'),
+    carsController = require('../controllers/cars-controller')(carsData),
+    passport = require('passport');
+
+// TODO: Add more routes.
+router
+    .get('/{id}', carsController.getCarById)
+    .post('/delete', passport.authenticate('bearer', {
+        session: false
+    }), carsController.removeCar);
+
+module.exports = function (app) {
+    app.use('/api/cars', router);
+};
