b98bcba35f347d9e35a01bd89763f41a53f0b697
Pavel <pawel.angelow@gmail.com>
2016-01-09 21:57:28 +0200

    App settings for using Jade rendering

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ var express = require('express'),
     port = process.env.PORT || 9001,
     connectionString = 'mongodb://localhost/telerik-racer';
 
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
+
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
