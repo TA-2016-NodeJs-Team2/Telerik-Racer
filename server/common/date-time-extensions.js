e68d77b451b0ec0b32099065b041de02499f76b7
mr.krustev <mr.krustev1994@gmail.com>
2016-01-11 18:15:22 +0200

    UTCDate extension fix.

--- server/common/date-time-extensions.js
+++ server/common/date-time-extensions.js
@@ -1,6 +1,6 @@
 module.exports = {
     getUTCDate: function (date) {
-        var year = date.getYear();
+        var year = date.getFullYear();
         var month = date.getMonth();
         var currDate = date.getDate();
         var hours = date.getHours();
